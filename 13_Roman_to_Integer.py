7b3c0754b6cb921fc61dae07715873b16b5c5717
yxliaoyx <yxliaoyx@gmail.com>
2023-01-13 22:16:40 +0800

    Create 13_Roman_to_Integer.py

---
+++ 13_Roman_to_Integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbols = [
+            ('M', 1000),
+            ('D', 500),
+            ('C', 100),
+            ('L', 50),
+            ('X', 10),
+            ('V', 5),
+            ('I', 1)
+        ]
+
+        result = 0
+
+        s = s.replace('IV', 'IIII').replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX').replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC').replace('CM', 'DCCCC')
+
+        for symbol, value in symbols:
+            result += s.count(symbol) * value
+
+        return result
