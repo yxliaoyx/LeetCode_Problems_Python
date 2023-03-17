cbd17592bfd46eaefd61553c95fb4cbb23cab911
yxliaoyx <yxliaoyx@gmail.com>
2023-03-17 22:17:34 +0800

    Create 76_Minimum_Window_Substring.py

---
+++ 10_99/76_Minimum_Window_Substring.py
@@ -0,0 +1,30 @@
+import collections
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)
+        missing = len(t)
+        start = 0
+        end = 0
+        i = 0
+
+        for j, char in enumerate(s, 1):
+            if need[char] > 0:
+                missing -= 1
+
+            need[char] -= 1
+
+            if missing == 0:
+                while i < j and need[s[i]] < 0:
+                    need[s[i]] += 1
+                    i += 1
+
+                if end == 0 or j - i < end - start:
+                    start, end = i, j
+
+                missing += 1
+                need[s[i]] += 1
+                i += 1
+
+        return s[start:end]
