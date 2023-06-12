e123778142570912a48076a2a8f73da929d386fa
yxliaoyx <yxliaoyx@gmail.com>
2023-06-12 22:08:41 +0800

    Create 169_Majority_Element.py

---
+++ 100_199/169_Majority_Element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        result = count = 0
+
+        for i in nums:
+            if count == 0:
+                result = i
+
+            if i == result:
+                count += 1
+            else:
+                count -= 1
+
+        return result
