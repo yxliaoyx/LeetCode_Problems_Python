d4ff8b360353bcb55263d97eaa9779db46a6c138
yxliaoyx <yxliaoyx@gmail.com>
2023-07-02 14:29:45 +0800

    Create 207_Course_Schedule.py

---
+++ 200_299/207_Course_Schedule.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        in_degree = [0] * numCourses
+        adjacency = [[] for _ in range(numCourses)]
+
+        for prerequisite in prerequisites:
+            adjacency[prerequisite[1]].append(prerequisite[0])
+            in_degree[prerequisite[0]] += 1
+
+        queue = [i for i in range(numCourses) if in_degree[i] == 0]
+
+        while queue:
+            for neighbor in adjacency[queue.pop(0)]:
+                in_degree[neighbor] -= 1
+
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return sum(in_degree) == 0
