993cc6a0e728fb08fe5dcd8bff723bd2ddcca283
yxliaoyx <yxliaoyx@gmail.com>
2023-09-21 22:09:55 +0800

    Create 332_Reconstruct_Itinerary.py

---
+++ 300_399/332_Reconstruct_Itinerary.py
@@ -0,0 +1,20 @@
+import collections
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = collections.defaultdict(list)
+
+        for start, end in sorted(tickets, reverse=True):
+            graph[start].append(end)
+
+        route = []
+        stack = ['JFK']
+
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop())
+
+            route.append(stack.pop())
+
+        return route[::-1]
