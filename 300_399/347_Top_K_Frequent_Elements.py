5a7f71abd71fbc8b31f77ba2d8f7f3784a952ccf
yxliaoyx <yxliaoyx@gmail.com>
2023-10-02 22:43:28 +0800

    Create 347_Top_K_Frequent_Elements.py

---
+++ 300_399/347_Top_K_Frequent_Elements.py
@@ -0,0 +1,13 @@
+import heapq
+
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return nums
+
+        count = Counter(nums)
+
+        return heapq.nlargest(k, count.keys(), key=count.get)
