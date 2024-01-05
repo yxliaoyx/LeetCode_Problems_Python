0a8c8829a254cc077c4c2dc2a4b591234af395b2
yxliaoyx <yxliaoyx@gmail.com>
2024-01-05 21:26:18 +0800

    Create 458_Poor_Pigs.py

---
+++ 400_499/458_Poor_Pigs.py
@@ -0,0 +1,6 @@
+from math import ceil, log2
+
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        return ceil(log2(buckets) / log2(minutesToTest // minutesToDie + 1))
