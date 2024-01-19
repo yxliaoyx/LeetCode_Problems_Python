46d5862825750404eabb22647eae0caa6e1225ae
yxliaoyx <yxliaoyx@gmail.com>
2024-01-19 21:40:41 +0800

    Create 474_Ones_and_Zeroes.py

---
+++ 400_499/474_Ones_and_Zeroes.py
@@ -0,0 +1,18 @@
+from functools import cache
+
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        counter = [(s.count("0"), s.count("1")) for s in strs]
+
+        @cache
+        def dp(i, j, idx):
+            if i < 0 or j < 0:
+                return -1
+
+            if idx == len(strs):
+                return 0
+
+            return max(dp(i, j, idx + 1), dp(i - counter[idx][0], j - counter[idx][1], idx + 1) + 1)
+
+        return dp(m, n, 0)
