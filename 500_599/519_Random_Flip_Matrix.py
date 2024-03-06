ef338d27ce886c7632be53c7458597fe8e7ba82f
yxliaoyx <yxliaoyx@gmail.com>
2024-03-06 21:54:32 +0800

    Create 519_Random_Flip_Matrix.py

---
+++ 500_599/519_Random_Flip_Matrix.py
@@ -0,0 +1,26 @@
+from random import randrange
+
+
+class Solution:
+    def __init__(self, m: int, n: int):
+        self.rows = m
+        self.cols = n
+        self.flipped = set()
+
+    def flip(self) -> List[int]:
+        while True:
+            i = randrange(self.rows)
+            j = randrange(self.cols)
+
+            if (i, j) not in self.flipped:
+                self.flipped.add((i, j))
+                return (i, j)
+
+    def reset(self) -> None:
+        self.flipped = set()
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(m, n)
+# param_1 = obj.flip()
+# obj.reset()
