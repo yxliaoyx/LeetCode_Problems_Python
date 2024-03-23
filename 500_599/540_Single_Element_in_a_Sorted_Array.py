bbd76e0447addeeb8e1912b9b89fbeebdbadc4d2
yxliaoyx <yxliaoyx@gmail.com>
2024-03-23 21:50:32 +0800

    Create 540_Single_Element_in_a_Sorted_Array.py

---
+++ 500_599/540_Single_Element_in_a_Sorted_Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 2
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == nums[mid ^ 1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return nums[left]
