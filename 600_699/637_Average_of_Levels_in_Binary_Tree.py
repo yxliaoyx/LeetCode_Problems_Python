649e6254529c6cf5d0d10f15b24616c78eecfcbf
yxliaoyx <yxliaoyx@gmail.com>
2024-05-22 22:16:20 +0800

    Create 637_Average_of_Levels_in_Binary_Tree.py

---
+++ 600_699/637_Average_of_Levels_in_Binary_Tree.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        queue = deque([root])
+        result = []
+
+        while queue:
+            level_sum = 0
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level_sum / level_size)
+
+        return result
