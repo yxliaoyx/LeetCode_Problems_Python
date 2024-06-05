f1bd8dcf87050191976156cdbead487806322ed6
yxliaoyx <yxliaoyx@gmail.com>
2024-06-05 21:58:03 +0800

    Create 650_2_Keys_Keyboard.py

---
+++ 600_699/650_2_Keys_Keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        steps = 0
+
+        for i in range(2, n + 1):
+            while n % i == 0:
+                steps += i
+                n //= i
+
+        return steps
