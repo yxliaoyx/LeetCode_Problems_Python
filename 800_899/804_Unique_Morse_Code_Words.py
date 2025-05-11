04e03a97df82be9f8a12068e31b266af542674e5
yxliaoyx <yxliaoyx@gmail.com>
2025-05-11 13:49:51 +0800

    Create 804_Unique_Morse_Code_Words.py

---
+++ 800_899/804_Unique_Morse_Code_Words.py
@@ -0,0 +1,31 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = [
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--..",
+        ]
+        return len({"".join(morse[ord(c) - ord("a")] for c in word) for word in words})
