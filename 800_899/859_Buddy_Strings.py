22a775c4b9322f51ef372807f3fa12af07989c8e
yxliaoyx <yxliaoyx@gmail.com>
2025-08-05 20:45:34 +0800

    Create 859_Buddy_Strings.py

---
+++ 800_899/859_Buddy_Strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+
+        if s == goal:
+            return len(s) > len(set(s))
+
+        diff = [(a, b) for a, b in zip(s, goal) if a != b]
+
+        return len(diff) == 2 and diff[0] == diff[1][::-1]
