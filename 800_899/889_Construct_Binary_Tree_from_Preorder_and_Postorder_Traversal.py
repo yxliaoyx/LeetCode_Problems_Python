985ba36c8506f910ee18b2da58a8ee9dfd82a8ac
yxliaoyx <yxliaoyx@gmail.com>
2025-09-30 21:22:21 +0800

    Update 889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py

--- 800_899/889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
+++ 800_899/889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
@@ -1,9 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        delta = (sum(aliceSizes) - sum(bobSizes)) // 2
-        set_b = set(bobSizes)
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not preorder:
+            return None
 
-        for a in aliceSizes:
-            b = a - delta
-            if b in set_b:
-                return [a, b]
+        root = TreeNode(preorder[0])
+
+        if len(preorder) == 1:
+            return root
+
+        i = postorder.index(preorder[1]) + 1
+
+        root.left = self.constructFromPrePost(preorder[1 : i + 1], postorder[:i])
+        root.right = self.constructFromPrePost(preorder[i + 1 :], postorder[i:-1])
+
+        return root
