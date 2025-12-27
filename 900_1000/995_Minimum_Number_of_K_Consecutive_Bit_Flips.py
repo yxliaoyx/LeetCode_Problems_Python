bea742f7cc544bd2ff6dda5fccdd3b183adc741e
yxliaoyx <yxliaoyx@gmail.com>
2025-12-27 16:03:15 +0800

    Update 995_Minimum_Number_of_K_Consecutive_Bit_Flips.py

--- 900_1000/995_Minimum_Number_of_K_Consecutive_Bit_Flips.py
+++ 900_1000/995_Minimum_Number_of_K_Consecutive_Bit_Flips.py
@@ -1,17 +1,16 @@
 class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         answer = count = 0
-        flipped = [0] * len(nums)
 
-        for i, num in enumerate(nums):
-            if i >= k:
-                count -= flipped[i - k]
+        for i in range(len(nums)):
+            if i >= k and nums[i - k] > 1:
+                count -= 1
 
-            if (num + count) % 2 == 0:
+            if (nums[i] + count) % 2 == 0:
                 if i + k > len(nums):
                     return -1
 
-                flipped[i] = 1
+                nums[i] = 2
                 count += 1
                 answer += 1
 
